ca4d1b47002fb5cb3e9ed97f88fdb1583d33e3ed
SAT <novikov.andrey.v@hotmail.com>
2020-06-06 17:51:20 +0300

    Add highcharts

--- Test Project Requirements/ClientApp/src/app/app.module.ts
+++ Test Project Requirements/ClientApp/src/app/app.module.ts
@@ -3,21 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SaleHighchartsComponent } from './Sales/sales.component';
+import { HighchartsChartComponent } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HighchartsChartComponent,
+    SaleHighchartsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: SaleHighchartsComponent, pathMatch: 'full' },
     ])
   ],
   providers: [],
