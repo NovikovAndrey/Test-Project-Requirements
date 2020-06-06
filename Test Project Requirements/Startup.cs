ed25476f1871dda7d2f41581bd79dc5c610a08a6
SAT <novikov.andrey.v@hotmail.com>
2020-06-06 22:22:42 +0300

    Good Test

--- Test Project Requirements/Startup.cs
+++ Test Project Requirements/Startup.cs
@@ -59,7 +59,7 @@ namespace Test_Project_Requirements
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                    pattern: "api/{controller}/{action=Get}/{id?}");
             });
 
             app.UseSpa(spa =>
