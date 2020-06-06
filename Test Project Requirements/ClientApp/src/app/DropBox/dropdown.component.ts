12818c986dde42c72395056fed3aa9a1a3533d51
SAT <novikov.andrey.v@hotmail.com>
2020-06-07 01:30:26 +0300

    Test

---
+++ Test Project Requirements/ClientApp/src/app/DropBox/dropdown.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngbd-dropdown',
+  templateUrl: './dropdown.component.html'
+})
+export class NgbdDropdown {
+}
--- Test Project Requirements/ClientApp/src/app/app.module.ts
+++ Test Project Requirements/ClientApp/src/app/app.module.ts
@@ -7,23 +7,30 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SaleHighchartsComponent } from './Sales/sales.component';
 import { HighchartsChartComponent } from 'highcharts-angular';
+import { NgbdDropdown } from './DropBox/dropdown.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HighchartsChartComponent,
-    SaleHighchartsComponent
+    SaleHighchartsComponent,
+    NgbdDropdown
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    NgbModule,
     RouterModule.forRoot([
       { path: '', component: SaleHighchartsComponent, pathMatch: 'full' },
     ])
   ],
+  exports: [
+    NgbdDropdown
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, NgbdDropdown]
 })
 export class AppModule { }
