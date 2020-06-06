6eeb265a85ca8ba76b8fa0b5e0ee3fdef2b48ea0
SAT <novikov.andrey.v@hotmail.com>
2020-06-06 23:26:48 +0300

    It works!!!

--- Test Project Requirements/ClientApp/src/app/Sales/sales.component.ts
+++ Test Project Requirements/ClientApp/src/app/Sales/sales.component.ts
@@ -30,97 +30,79 @@ export class SaleHighchartsComponent implements OnInit{
 
   loadSales() {
     this.dataService.getProducts()
-      .subscribe((data: SaleModel[]) => this.sales = data);
-    this.XArgs = new Array<string>()
-    this.Column = new Array<number>();
-    this.Line = new Array<number>();
-    for (var i = 0, len = this.sales.length; i < len; i++) {
-      this.XArgs[i] = this.sales[i].dateSale.toString();
-      this.Column[i] = +(this.sales[i].amountDollars / 1000).toFixed(2);
-      this.Line[i] = this.sales[i].sale;
-    }
+      .subscribe((data: SaleModel[]) => {
+        this.sales = data;
+        this.XArgs = new Array<string>();
+        this.Column = new Array<number>();
+        this.Line = new Array<number>();
+        for (var i = 0, len = this.sales.length; i < len; i++) {
+          this.XArgs[i] = this.sales[i].dateSale.toString();
+          this.Column[i] = +(this.sales[i].amountDollars / 1000).toFixed(2);
+          this.Line[i] = this.sales[i].sale;
+        }
+        Highcharts.chart('SalesChart', {
+          chart: { zoomType: 'xy' },
+          title: { text: this.title },
+          xAxis: { categories: this.XArgs, crosshair: true },
+          yAxis: [
+            { // Primary yAxis
+              title: {
+                text: this.Y1,
+                style: {
+                  color: Highcharts.getOptions().colors[1]
+                }
+              }
+            },
+            { // Secondary yAxis
+              title: {
+                text: '',
+                style: {
+                  color: Highcharts.getOptions().colors[0]
+                }
+              },
+              labels: {
+                style: {
+                  color: Highcharts.getOptions().colors[0]
+                }
+              },
+              opposite: true
+            }
+          ],
+          legend: {
+            layout: 'vertical',
+            align: 'right',
+            x: -100,
+            verticalAlign: 'top',
+            y: 0,
+            floating: true
+          },
+          series: [
+            {
+              name: this.ColumnLegend,
+              type: 'column',
+              data: this.Column,
+            },
+            {
+              name: this.LineLegend,
+              type: 'spline',
+              yAxis: 1,
+              data: this.Line,
+            }
+          ],
+          tooltip: {
+            shared: true
+          }
+        });
+      });
+    //this.XArgs = new Array<string>()
+    //this.Column = new Array<number>();
+    //this.Line = new Array<number>();
+    //for (var i = 0, len = this.sales.length; i < len; i++) {
+    //  this.XArgs[i] = this.sales[i].dateSale.toString();
+    //  this.Column[i] = +(this.sales[i].amountDollars / 1000).toFixed(2);
+    //  this.Line[i] = this.sales[i].sale;
+    //}
+   
   }
-  chartOptions = {
-    chart: {
-      zoomType: 'xy'
-    },
-    title: {
-      text: 'Source: WorldClimate.com'
-    },
-    subtitle: {
-      text: 'Average Monthly Temperature and Rainfall in Tokyo'
-    },
-    xAxis: {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      crosshair: true
-    },
-    yAxis: [
-      { // Primary yAxis
-        labels: {
-          format: '{value}\xB0C',
-          style: {
-            color: Highcharts.getOptions().colors[1]
-          }
-        },
-        title: {
-          text: 'Temperature',
-          style: {
-            color: Highcharts.getOptions().colors[1]
-          }
-        }
-      },
-      { // Secondary yAxis
-        title: {
-          text: 'Rainfall',
-          style: {
-            color: Highcharts.getOptions().colors[0]
-          }
-        },
-        labels: {
-          format: '{value} mm',
-          style: {
-            color: Highcharts.getOptions().colors[0]
-          }
-        },
-        opposite: true
-      }
-    ],
-    tooltip: {
-      shared: true
-    },
-    legend: {
-      layout: 'vertical',
-      align: 'left',
-      x: 60,
-      verticalAlign: 'top',
-      y: 100,
-      floating: true
-      //,
-
-      //backgroundColor: (
-      //  Highcharts.theme && Highcharts.theme.legendBackgroundColor)
-      //  || '#FFFFFF'
-    },
-    series: [
-      {
-        name: 'Rainfall',
-        type: 'column',
-        yAxis: 1,
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5,
-          216.4, 194.1, 95.6, 54.4],
-        tooltip: {
-          valueSuffix: ' mm'
-        }
-      },
-      {
-        name: 'Temperature',
-        type: 'spline',
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
-        tooltip: {
-          valueSuffix: '\xB0C'
-        }
-      }
-    ]
-  };
+  
 }
