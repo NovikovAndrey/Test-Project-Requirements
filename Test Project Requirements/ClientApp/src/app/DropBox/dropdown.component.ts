64a354b866a340e3bb628bc2fdc8c8e0c3eb7778
SAT <novikov.andrey.v@hotmail.com>
2020-06-07 22:23:59 +0300

    Clear

--- Test Project Requirements/ClientApp/src/app/DropBox/dropdown.component.ts
+++ Test Project Requirements/ClientApp/src/app/DropBox/dropdown.component.ts
@@ -5,36 +5,17 @@ import { SaleHighchartsComponent } from '../Sales/sales.component';
 @Component({
   selector: 'ngbd-dropdown',
   templateUrl: './dropdown.component.html',
-//  template: `<p>dsfsdfsdfs</p><div class="row">
-//    <div class="col">
-//      <div ngbDropdown class="d-inline-block">
-//        <button class="btn btn-outline-primary" id="dropdown"
-//                ngbDropdownToggle>
-//          {{ selectedValue.value || "Select Environment" }}
-//        </button>
-//        <div ngbDropdownMenu aria-labelledby="dropdown" #selectedValue>
-//          <div *ngFor="let name of names;">
-//            <button ngbDropdownItem id="option" on-click="GetGroup(selectedValue.value)" (click)="GetGroup(selectedValue.value)">
-//              {{name}}
-//            </button>
-//          </div>
-//        </div>
-//      </div>
-//    </div>`,
-////`<button (click)="change(true)"> +</button>
-////  //  < button(click)="change(false)"> -</button>`
   providers: [DataService]
 })
 export class NgbdDropdown implements OnInit{
   names: string[];
   comp: SaleHighchartsComponent;
   count: string;
-  @Output() onChanged = new EventEmitter<string>();
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.loadNames();    // загрузка данных при старте компонента  
+    this.loadNames();
   }
 
   loadNames() {
@@ -46,9 +27,6 @@ export class NgbdDropdown implements OnInit{
   };
 
   public GetGroup(increased: string) {
-  /* this.onChanged.emit(increased);*/
     this.dataService.getSales(increased)
-    //this.onChanged.emit(increased);
-    this.count = increased;
   };
 }
--- Test Project Requirements/ClientApp/src/app/Sales/sales.component.ts
+++ Test Project Requirements/ClientApp/src/app/Sales/sales.component.ts
@@ -7,92 +7,19 @@ import { NgbdDropdown } from '../DropBox/dropdown.component';
 @Component({
   selector: 'app-Highcharts',
   templateUrl: `./sales.component.html`,
-
   providers: [DataService]
 })
 
 export class SaleHighchartsComponent implements OnInit{
-  sale: SaleModel = new SaleModel();   // изменяемый товар
-  sales: SaleModel[];                // массив товаров
-  tableMode: boolean = true;
-  highcharts = Highcharts;
-  XArgs: string[] = new Array<string>();
-  Column: number[] = new Array<number>();
-  Line: number[] = new Array<number>();
-  //test: NgbdDropdown;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.loadSales();    // загрузка данных при старте компонента
+    this.loadSales();    
   }
 
   public loadSales() {
     this.dataService.getSales('Day');
-      //.subscribe((data: SaleModel[]) => {
-      //  this.sales = data;
-      //  for (var i = 0, len = this.sales.length; i < len; i++) {
-      //    this.XArgs[i] = this.sales[i].dateSale.toString();
-      //    this.Column[i] = +(this.sales[i].amountDollars / 1000).toFixed(2);
-      //    this.Line[i] = this.sales[i].sale;
-      //  }
-      //  Highcharts.chart('SalesChart', {
-      //    chart: { zoomType: 'xy' },
-      //    title: { text: this.title },
-      //    xAxis: { categories: this.XArgs, crosshair: true },
-      //    yAxis: [
-      //      { // Primary yAxis
-      //        title: {
-      //          text: this.Y1,
-      //          style: {
-      //            color: Highcharts.getOptions().colors[1]
-      //          }
-      //        }
-      //      },
-      //      { // Secondary yAxis
-      //        title: {
-      //          text: '',
-      //          style: {
-      //            color: Highcharts.getOptions().colors[0]
-      //          }
-      //        },
-      //        labels: {
-      //          style: {
-      //            color: Highcharts.getOptions().colors[0]
-      //          }
-      //        },
-      //        opposite: true
-      //      }
-      //    ],
-      //    legend: {
-      //      layout: 'vertical',
-      //      align: 'right',
-      //      x: -100,
-      //      verticalAlign: 'top',
-      //      y: 0,
-      //      floating: true
-      //    },
-      //    series: [
-      //      {
-      //        name: this.ColumnLegend,
-      //        type: 'column',
-      //        data: this.Column,
-      //      },
-      //      {
-      //        name: this.LineLegend,
-      //        type: 'spline',
-      //        yAxis: 1,
-      //        data: this.Line,
-      //      }
-      //    ],
-      //    tooltip: {
-      //      shared: true
-      //    }
-      //  });
-      //});
-  }
-  
-  onChanged(increased: any) {
-    increased == true ? this.loadSales() : false;
+     
   }
 }
