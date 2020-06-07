e79644342c52b464d02dc1394e371782eee2a949
SAT <novikov.andrey.v@hotmail.com>
2020-06-07 20:27:27 +0300

    It is Work

--- Test Project Requirements/ClientApp/src/app/Sales/sales.component.ts
+++ Test Project Requirements/ClientApp/src/app/Sales/sales.component.ts
@@ -15,10 +15,6 @@ export class SaleHighchartsComponent implements OnInit{
   sale: SaleModel = new SaleModel();   // изменяемый товар
   sales: SaleModel[];                // массив товаров
   tableMode: boolean = true;
-  title = 'app';
-  Y1 = 'Sum (In thousands)';
-  ColumnLegend = 'Sum $/K';
-  LineLegend = 'Sales';
   highcharts = Highcharts;
   XArgs: string[] = new Array<string>();
   Column: number[] = new Array<number>();
@@ -32,68 +28,68 @@ export class SaleHighchartsComponent implements OnInit{
   }
 
   public loadSales() {
-    this.dataService.getSales('Day')
-      .subscribe((data: SaleModel[]) => {
-        this.sales = data;
-        for (var i = 0, len = this.sales.length; i < len; i++) {
-          this.XArgs[i] = this.sales[i].dateSale.toString();
-          this.Column[i] = +(this.sales[i].amountDollars / 1000).toFixed(2);
-          this.Line[i] = this.sales[i].sale;
-        }
-        Highcharts.chart('SalesChart', {
-          chart: { zoomType: 'xy' },
-          title: { text: this.title },
-          xAxis: { categories: this.XArgs, crosshair: true },
-          yAxis: [
-            { // Primary yAxis
-              title: {
-                text: this.Y1,
-                style: {
-                  color: Highcharts.getOptions().colors[1]
-                }
-              }
-            },
-            { // Secondary yAxis
-              title: {
-                text: '',
-                style: {
-                  color: Highcharts.getOptions().colors[0]
-                }
-              },
-              labels: {
-                style: {
-                  color: Highcharts.getOptions().colors[0]
-                }
-              },
-              opposite: true
-            }
-          ],
-          legend: {
-            layout: 'vertical',
-            align: 'right',
-            x: -100,
-            verticalAlign: 'top',
-            y: 0,
-            floating: true
-          },
-          series: [
-            {
-              name: this.ColumnLegend,
-              type: 'column',
-              data: this.Column,
-            },
-            {
-              name: this.LineLegend,
-              type: 'spline',
-              yAxis: 1,
-              data: this.Line,
-            }
-          ],
-          tooltip: {
-            shared: true
-          }
-        });
-      });
+    this.dataService.getSales('Day');
+      //.subscribe((data: SaleModel[]) => {
+      //  this.sales = data;
+      //  for (var i = 0, len = this.sales.length; i < len; i++) {
+      //    this.XArgs[i] = this.sales[i].dateSale.toString();
+      //    this.Column[i] = +(this.sales[i].amountDollars / 1000).toFixed(2);
+      //    this.Line[i] = this.sales[i].sale;
+      //  }
+      //  Highcharts.chart('SalesChart', {
+      //    chart: { zoomType: 'xy' },
+      //    title: { text: this.title },
+      //    xAxis: { categories: this.XArgs, crosshair: true },
+      //    yAxis: [
+      //      { // Primary yAxis
+      //        title: {
+      //          text: this.Y1,
+      //          style: {
+      //            color: Highcharts.getOptions().colors[1]
+      //          }
+      //        }
+      //      },
+      //      { // Secondary yAxis
+      //        title: {
+      //          text: '',
+      //          style: {
+      //            color: Highcharts.getOptions().colors[0]
+      //          }
+      //        },
+      //        labels: {
+      //          style: {
+      //            color: Highcharts.getOptions().colors[0]
+      //          }
+      //        },
+      //        opposite: true
+      //      }
+      //    ],
+      //    legend: {
+      //      layout: 'vertical',
+      //      align: 'right',
+      //      x: -100,
+      //      verticalAlign: 'top',
+      //      y: 0,
+      //      floating: true
+      //    },
+      //    series: [
+      //      {
+      //        name: this.ColumnLegend,
+      //        type: 'column',
+      //        data: this.Column,
+      //      },
+      //      {
+      //        name: this.LineLegend,
+      //        type: 'spline',
+      //        yAxis: 1,
+      //        data: this.Line,
+      //      }
+      //    ],
+      //    tooltip: {
+      //      shared: true
+      //    }
+      //  });
+      //});
   }
   
   onChanged(increased: any) {
--- Test Project Requirements/ClientApp/src/app/data.service.ts
+++ Test Project Requirements/ClientApp/src/app/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HistorySaleModel } from './Models/HistorySaleModel';
+import { SaleModel } from './Models/SaleModel';
+import * as Highcharts from 'highcharts';
 
 @Injectable()
 export class DataService {
@@ -8,12 +10,83 @@ export class DataService {
   private urlSales = "/api/HistorySale";
   private urlNames = "api/DateGroupType";
   s: string = 'Day';
+    sales: SaleModel[];
+  title = 'app';
+  Y1 = 'Sum (In thousands)';
+  ColumnLegend = 'Sum $/K';
+  LineLegend = 'Sales';
+  XArgs: string[];
+  Column: number[];
+  Line: number[];
 
   constructor(private http: HttpClient) {
   }
 
   getSales(s: string) {
-    return this.http.get(this.urlSales + '/'+s);
+    this.XArgs = new Array<string>();
+    this.Column = new Array<number>();
+    this.Line = new Array<number>();
+    return this.http.get(this.urlSales + '/' + s).subscribe((data: SaleModel[]) => {
+      this.sales = data;
+      for (var i = 0, len = this.sales.length; i < len; i++) {
+        this.XArgs[i] = this.sales[i].dateSale.toString();
+        this.Column[i] = +(this.sales[i].amountDollars / 1000).toFixed(2);
+        this.Line[i] = this.sales[i].sale;
+      }
+      Highcharts.chart('SalesChart', {
+        chart: { zoomType: 'xy' },
+        title: { text: this.title },
+        xAxis: { categories: this.XArgs, crosshair: true },
+        yAxis: [
+          { // Primary yAxis
+            title: {
+              text: this.Y1,
+              style: {
+                color: Highcharts.getOptions().colors[1]
+              }
+            }
+          },
+          { // Secondary yAxis
+            title: {
+              text: '',
+              style: {
+                color: Highcharts.getOptions().colors[0]
+              }
+            },
+            labels: {
+              style: {
+                color: Highcharts.getOptions().colors[0]
+              }
+            },
+            opposite: true
+          }
+        ],
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          x: -100,
+          verticalAlign: 'top',
+          y: 0,
+          floating: true
+        },
+        series: [
+          {
+            name: this.ColumnLegend,
+            type: 'column',
+            data: this.Column,
+          },
+          {
+            name: this.LineLegend,
+            type: 'spline',
+            yAxis: 1,
+            data: this.Line,
+          }
+        ],
+        tooltip: {
+          shared: true
+        }
+      });
+    });;
   }
   getSales1(s: string) {
     return this.http.get(this.urlSales +'/group='+ s);
